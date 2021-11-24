0b15cb27495bc74d12fa5e9ff7e80bdbddd933d5
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-24 10:39:06 -0600

    npm packages implemented for additional security

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+
 const express = require('express');
 const app = express();
 
 // connect DB
-const connectDB = require('./db/connect')
-const authenticateUser = require ('./middleware/authentication')
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 
 // routers
 const authRouter = require('./routes/auth');
@@ -15,8 +23,19 @@ const jobRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// limit requests
+app.set('trust proxy', 1);
+app.use(rateLimit({
+  windowMs: 15 * 6 * 1000, // 15min
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
+// express set up
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xxs());
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
